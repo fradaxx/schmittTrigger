12ddcde0bcc1a437e6d682ad87befc3382852895
fradaxx <f.adamo@alice.it>
2021-01-27 17:52:12 +0100

    -

--- Examples/simpleDemo/__vm/.simpleDemo.vsarduino.h
+++ Examples/simpleDemo/__vm/.simpleDemo.vsarduino.h
@@ -1,9 +1,10 @@
 /* 
 	Editor: https://www.visualmicro.com/
-			visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
-			the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
-			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
-			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
+			This file is for intellisense purpose only.
+			Visual micro (and the arduino ide) ignore this code during compilation. This code is automatically maintained by visualmicro, manual changes to this file will be overwritten
+			The contents of the _vm sub folder can be deleted prior to publishing a project
+			All non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
+			Note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
 	Hardware: Arduino Due (Programming Port), Platform=sam, Package=arduino
 */
@@ -12,9 +13,8 @@
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define printf iprintf
 #define F_CPU 84000000L
-#define ARDUINO 10805
+#define ARDUINO 108013
 #define ARDUINO_SAM_DUE
 #define ARDUINO_ARCH_SAM
 #define __SAM3X8E__
@@ -22,6 +22,7 @@
 #define USB_PID 0x003e
 #define USBCON
 #define __cplusplus 201103L
+#define _Pragma(x)
 #define __ARM__
 #define __arm__
 #define __inline__
@@ -43,6 +44,8 @@ extern "C" void __cxa_pure_virtual() {;}
 
 #define __INTPTR_TYPE__ long
 #define __INT32_TYPE__ long
+#define __UINTPTR_TYPE__ long
+#define __UINT32_TYPE__ long
 #define _Pragma(x) pragma
 
 
@@ -50,7 +53,7 @@ extern "C" void __cxa_pure_virtual() {;}
 	#include "sam3xa.h"
 #endif
 
-#include <arduino.h>
+#include "arduino.h"
 #include <pins_arduino.h> 
 #include <variant.h> 
 #include <variant.cpp> 
